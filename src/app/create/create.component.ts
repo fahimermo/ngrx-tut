2b99ad6fa661e9cbd311d95e5e01377efc3e73c3
fahimermo <elegantmoon.tech@gmail.com>
2021-04-27 05:19:13 +0200

    Update create component ts

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+import { AppState } from './../app.state';
+import { Tutorial } from './../models/tutorial.model';
+import * as TutorialActions from './../actions/tutorial.actions';
+
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent implements OnInit {
+  constructor(private store: Store<AppState>) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  addTutorial(name, url) {
+    this.store.dispatch(
+      new TutorialActions.AddTutorial({ name: name, url: url })
+    );
   }
 
+  ngOnInit(): void {}
 }
