6a6693d16146728f4dc746db88902689034bcdb5
fahimermo <elegantmoon.tech@gmail.com>
2021-04-27 05:10:38 +0200

    Update read component

--- src/app/read/read.component.ts
+++ src/app/read/read.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { Tutorial } from './../models/tutorial.model';
+import { AppState } from './../app.state';
 
 @Component({
   selector: 'app-read',
   templateUrl: './read.component.html',
-  styleUrls: ['./read.component.scss']
+  styleUrls: ['./read.component.scss'],
 })
 export class ReadComponent implements OnInit {
+  tutorials: Observable<Tutorial[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private store: Store<AppState>) {
+    this.tutorials = store.select('tutorial');
   }
 
+  ngOnInit(): void {}
 }
